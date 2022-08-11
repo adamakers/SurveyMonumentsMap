cebd58808b91ab62bcddb2d3eda26e07fe5d185d
Adam Akers <adamrakers@gmail.com>
2022-08-10 23:29:01 -0600

    added the sidebar layout in MapPage.  Sidebar component not added yet

--- src/content/MapPage.js
+++ src/content/MapPage.js
@@ -1,15 +1,25 @@
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { Row, Col } from 'reactstrap';
+import { Button } from 'reactstrap';
+import MapSidebar from './MapSidebar';
 import 'leaflet/dist/leaflet.css'
+
 
 const MapPage = () => {
   return (
-    <MapContainer id="map" center={[38.9628, -104.7886]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-    </MapContainer>
+    <Row className='map-page-content g-0'>
+      <Col xs='8' style={{padding: 0}}>
+        <MapContainer id="map" center={[38.9628, -104.7886]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
+      </Col>
+      <Col xs='4' style={{padding: 0}}>
+        <MapSidebar />
+      </Col>
+    </Row>
   );
 }
 
---
+++ src/content/MapSidebar.js
@@ -0,0 +1,12 @@
+const MapSidebar = () => {
+  return (
+    <div className='sidebar'>
+      aksljflkasjf
+
+      {/* CREATE A CARD COMPONENT */}
+      {/* ADJUST WIDTH WITH BS SOMEHOW */}
+    </div>
+  );
+}
+
+export default MapSidebar;
